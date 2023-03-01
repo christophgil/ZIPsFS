38ecf2f92da9a6ff06e491b56e1f58991ae5f63e
Christoph Gille <christophgil@gmail.com>
2023-03-01 08:41:08 +0000

    Something already works

--- Fuse_make_example.sh
+++ Fuse_make_example.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 set -u
 readonly B=~/test/fuse
-mkdir -p  $B{rootdir_writable,mnt}
+mkdir -p  ${B}/{rootdir_writable,mnt}
 main(){
     local j count=0 f
 
     for((j=1;j<4;j++));do
-
-        mkdir -p $B/root_$j/subdir1/subdir2/lastsubdir
-        for d in $(find ~/test/fuse/branch1/ -type d)    ;do
+        local base=$B/root_$j
+        mkdir -p $base/subdir1/subdir2/lastsubdir
+        for d in $(find $base -type d)    ;do
             f=$d/file$count.txt
             [[ ! -f $f ]] && echo hello world $j > $f
             ((count++))
         done
     done
-    f=root_2/subdir1/verybig.img
+    f=$base/verybig.img
     [[ ! -f $f ]] && fallocate -l 500M $f
 
-    f=root_2/subdir1/big.img
+    f=$base/big.img
     [[ ! -f $f ]] && seq 1000 > $f
-
 
 }
 main
+find $B -ls
---
+++ test_ZIPsFS.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+rm core 2>/dev/null
+
+
+
+readonly CP=/proc/sys/kernel/core_pattern
+if [[ core != $(<$CP) ]];then
+
+    echo Going to change core_pattern ...
+    sudo tee $CP <<< core
+
+fi
+false &&  ls -l $CP &&  more $CP
+
+ulimit -c unlimited
+
+
+
+sudo umount ~/test/fuse/mnt/
+/home/cgille/git_projects/ZIPsFS/src/ZIPsFS  -f ~/test/fuse/rootdir_writable/ ~/test/fuse/root{1,2,3}  ~/test/fuse/mnt
+
+ls -l core 2>/dev/null
