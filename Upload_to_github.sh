8969f877c760e5c3c03ee5e9c7f3ee2dd723508c
Christoph Gille <christophgil@gmail.com>
2023-07-05 12:23:13 +0000

    Added man page

--- Upload_to_github.sh
+++ Upload_to_github.sh
@@ -13,29 +13,39 @@ make_readme(){
     cat <<EOF
     # ZIPsFS - FUSE-based  overlay file system which expands  ZIP files
 
+# CURRENT STATE
+
+Usable.
+
+However, we are still fixing minor bugs.
+
 # MOTIVATION
 
-We use closed-source proprietary Windows software and shared libraries for reading  experimental data from mass spectrometry machines.
+We use closed-source proprietary Windows software and shared libraries for reading experimental data from different types of mass spectrometry machines.
 Unfortunately, the sheer amount of file data brought our high-performance Windows server to a stand still.
 Currently we are using  Wine on Ubuntu which works much better for large files.
-Our data is archived in a read-only WORM file system. To reduce the number of individual files,  all files of one  record are bundled in one ZIP.
+Most data is archived in a read-only WORM file system for years.
 
-We hoped that we could use pipes or process substitution to access the data inside the ZIP, however this is not possible for the  software we are using.
-However, mounting all ZIP files before starting the computation worked well in the past.
+To reduce the number of individual files,  files of one record are bundled in one ZIP.
+We hoped that the data inside the ZIP was easily accessible using  pipes or process substitution. Unfortunately, this is not the case  for the proprietary  software we are using.
+Another approach, mounting all ZIP files before starting the computation worked well in the past.
 
 ZIPsFS has been developed to solve the following  problems:
 
 - Recently  the size of our experiments and thus the number of ZIP files grew enormously.
-  Mounting thousands of individual ZIP files at the same time is not feasible.
+  Mounting thousands of individual ZIP files at a time is not feasible.
 
-- Some proprietary  software for mass spectrometry software requires write access for the volume where the data is loaded from.
+- Some proprietary  software for mass spectrometry software requires write access for the volume where the files are loaded from.
 
-- Some of the  files are  not read  sequentially. Instead data is read  from varying file positions. This is particularly  inefficient
+- Some of the  files are  not read  sequentially. Instead data is read  from different file positions. This is particularly  inefficient
   for compressed  ZIP entries. The worst case is a  negative i.e. backward  seek operation.
   It  will result in a new remote request from the beginning of the member content.
 
-- When experimental records  are archived the files are first stored in an  intermediate storage as a ZIP file. Later after verification, these ZIP files are moved to the WORM file system.
-  Consequently there are different places where files could be located.
+- Experimental records are first stored in an intermediate storage as a ZIP file. Later after verification, these ZIP files are then moved to the WORM file system which acts as a final secure archive.
+  Consequently there are different places to look for a particular file.
+
+- With one  proprietary shared library to read the data, millions of redundant requests to the file system occur. This
+  is usually not a  problem  when records are stored locally as flat files.  However, for remote file access  or mounted ZIP files there is a strong   performance loss.
 
 EOF
     echo
