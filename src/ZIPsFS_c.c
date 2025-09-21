0380c991040bcb407d922d1a0abe483621d92be5
Christoph Gille <christophgil@gmail.com>
2025-09-21 15:58:22 +0000

    web files as reg files

---
+++ src/ZIPsFS_c.c
@@ -0,0 +1,108 @@
+/////////////////////////////////////////////////////////////
+/// COMPILE_MAIN=ZIPsFS                                   ///
+/// Dynamically generated files                           ///
+/// This file can be customized by the user               ///
+/////////////////////////////////////////////////////////////
+#define FSIZE_FROM_HASHTABLE(st,vp,vp_l,default)  if (0>=(st->st_size=fsize_from_hashtable((st->st_ino=inode_from_virtualpath(vp,vp_l))))) st->st_size=default
+#define FSIZE_TO_HASHTABLE(bb,vp,vp_l)  fsize_to_hashtable(vp,vp_l, bb[0]?textbuffer_length(bb[0]):0)
+#define ZIPSFS_C_IS_DIR_A (1<<0)
+#define ZIPSFS_C_MMAP (1<<1)
+typedef struct textbuffer** c_read_handle_t;
+
+
+
+
+
+
+static int c_from_exec_output(struct textbuffer **bb,const uint8_t flags,char *cmd[],char *env[]){
+
+    return textbuffer_from_exec_output((flags&ZIPSFS_C_MMAP)?TXTBUFSGMT_MUNMAP:0,
+                                       _zipsfs_c_init_tb(bb),
+                                       (const char*const*)cmd,(const char*const*)env,NULL);
+}
+#define X(...)  c_from_exec_output(__VA_ARGS__)
+#define H(b,txt,size) textbuffer_add_segment(0,                  _zipsfs_c_init_tb(b),txt,size);
+#define M(b,txt,size) textbuffer_add_segment(TXTBUFSGMT_MUNMAP,  _zipsfs_c_init_tb(b),txt,size);
+#define C(b,txt,size) textbuffer_add_segment(TXTBUFSGMT_NO_FREE, _zipsfs_c_init_tb(b),txt,size);
+#include "ZIPsFS_configuration_c.c"
+#undef H
+#undef M
+#undef C
+#undef X
+
+#define a(vp,vp_l)   const bool inDirA=IS_IN_DIR_AUTOGEN(vp,vp_l)
+#define o()   (inDirA?DIR_AUTOGEN_L:0)
+#define f()   inDirA?ZIPSFS_C_IS_DIR_A:0
+
+
+static struct textbuffer *_zipsfs_c_init_tb(struct textbuffer **bb){
+  if (!bb[0]){
+    cg_thread_assert_not_locked(mutex_fhandle);
+    bb[0]=textbuffer_new(COUNT_MALLOC_MEMCACHE_TXTBUF);
+  }
+  return bb[0];
+}
+
+
+static  int64_t c_fuse_open(const struct zippath *zpath){
+  a(VP(),VP_L());
+  if (!config_c_open(f(),VP()+o(),VP_L()-o())) return 0;
+  int64_t fh;
+  LOCK(mutex_fhandle,fhandle_create(FHANDLE_FLAG_IS_CCODE,(fh=next_fh()),zpath));
+  return fh;
+}
+static bool c_getattr(struct stat *st, const char *vp,const int vp_l){
+  a(vp,vp_l);
+  stat_init(st,0,NULL);
+  st->st_mtime=time(NULL);
+  if (!config_c_getattr(f(),vp+o(),vp_l-o(),st)) return false;
+  if (st->st_mode&S_IFDIR) stat_set_dir(st);
+  if (!st->st_ino) st->st_ino=inode_from_virtualpath(vp+o(),vp_l-o());
+  return true;
+}
+
+static void c_file_file_content_to_fhandle(struct fHandle *d){
+  if (!(d->flags&FHANDLE_FLAG_IS_CCODE)) return;
+  //    if (d->memcache && d->memcache->txtbuf){ d->flags|=FHANDLE_FLAG_NOT_CCODE; return;}
+  const char *vp=D_VP(d);
+  const int vp_l=D_VP_L(d);
+  a(vp,vp_l);
+  struct textbuffer *bb[1]={0};
+  if (config_c_read(bb,f(),vp+o(), vp_l-o())){
+    assert(bb[0]);
+    lock(mutex_fhandle);
+    if (!fhandle_set_text(d,bb[0])){
+      FREE_NULL_MALLOC_ID(bb[0]);
+    }else{
+      d->memcache->txtbuf=bb[0];
+      memcache_set_status(d,memcache_done);
+      d->flags|=FHANDLE_FLAG_MEMCACHE_COMPLETE;
+    }
+    unlock(mutex_fhandle);
+  }
+}
+static void c_readdir(const struct zippath *zpath,void *buf, fuse_fill_dir_t filler,struct ht *no_dups){
+  a(VP(),VP_L());
+  //log_entered_function("%s inDirA: %d",VP(),inDirA);
+  char fname[MAX_PATHLEN+1];
+  bool isDirectory[1];
+  FOR(i,0,ZIPSFS_C_MAX_NUM){
+    *fname=0;
+    if (!config_c_readdir(f(),VP()+o(),VP_L()-o(),i,fname,MAX_PATHLEN,isDirectory)) break;
+    if (*fname){
+      struct stat st={0};
+      stat_init(&st,*isDirectory?-1:0,NULL);
+      st.st_ino=inode_from_virtualpath(VP()+o(),VP_L()-o());
+      filler_add_no_dups(filler,buf, fname, &st,NULL /*no_dups*/);
+    }
+
+  }
+}
+
+
+
+
+
+#undef f
+#undef a
+#undef o
---
+++ src/ZIPsFS_configuration_c.c
@@ -0,0 +1,92 @@
+/////////////////////////////////////////////////////////////
+/// COMPILE_MAIN=ZIPsFS                                   ///
+/// Dynamically generated files                           ///
+/// This file can be customized by the user               ///
+/////////////////////////////////////////////////////////////
+// See ZIPsFS_c.c  WITH_CCODE
+#define ZIPSFS_C_MAX_NUM 10 /* Maximum number of synthetic file definitions */
+#define CONFIG_C_EXAMPLE_DIR "example_generated_file"
+#define CONFIG_C_EXAMPLE     "example_generated_file.txt"
+#define CONFIG_C_EXAMPLE_DIR_L (sizeof(CONFIG_C_EXAMPLE_DIR)-1)
+#define CONFIG_C_EXAMPLE_L     (sizeof(CONFIG_C_EXAMPLE)-1)
+#define CONFIG_C_IS_EXAMPLE(vp,vp_l)  (vp_l==1+CONFIG_C_EXAMPLE_DIR_L+1+CONFIG_C_EXAMPLE_L && vp[1+CONFIG_C_EXAMPLE_DIR_L]=='/' && !strcmp(vp+1,CONFIG_C_EXAMPLE_DIR"/"CONFIG_C_EXAMPLE))
+
+static bool config_c_open(const int flags, const char *vp, const int vp_l){
+  if (CONFIG_C_IS_EXAMPLE(vp,vp_l)) return true;
+  return false;
+}
+static bool config_c_getattr(const int flags, const char *vp, const int vp_l, struct stat *st){
+  if (vp_l-1==CONFIG_C_EXAMPLE_DIR_L && !strcmp(vp+1,CONFIG_C_EXAMPLE_DIR)){
+    st->st_mode|=S_IFDIR;
+    return true;
+  }
+  if (CONFIG_C_IS_EXAMPLE(vp,vp_l)){
+    FSIZE_FROM_HASHTABLE(st,vp,vp_l, 999);
+    st->st_mtime=time(NULL);
+    return true;
+  }
+  return false;
+}
+
+static bool config_c_readdir(const int flags,const char *vp, const int vp_l,const int iTh, char *name, const int path_max,bool *isDirectory){
+  assert(iTh<ZIPSFS_C_MAX_NUM);
+  const char *n=NULL;
+  switch(iTh){
+  case 0:
+    if (!vp_l){
+      n=CONFIG_C_EXAMPLE_DIR;
+      *isDirectory=true;
+    }
+    break;
+  case 1:
+    if (vp_l==CONFIG_C_EXAMPLE_DIR_L+1 && !strcmp(vp+1,CONFIG_C_EXAMPLE_DIR)) n=CONFIG_C_EXAMPLE;
+    break;
+  default: return false;
+  }
+  if (n)strcpy(name,n);
+  return true;
+}
+
+
+
+
+static bool config_c_read(c_read_handle_t handle,const int flags,const char *vp, const int vp_l){
+  if (CONFIG_C_IS_EXAMPLE(vp,vp_l)){
+    const char *src=__FILE__;
+    const char
+      *c="This string will not be destroyed with free() or munmap().\n",
+      *h="This string is on the heap and will be removed with free().\n",
+      *m="This string is stored as an (annonymous) memory mapped file and will be destroyed with munmap().\n";
+    C(handle,ANSI_INVERSE"DEMONSTRATING DYNAMICALLY GENERATED FILE CONTENT"ANSI_RESET"\n\nUsers can implement file generation by customizing the source file "ANSI_FG_BLUE,0);
+    C(handle,src+1+cg_last_slash(src),0);
+    C(handle,ANSI_RESET" and recompilation.\n\n",0);
+    C(handle,"The following demonstrates three storage methods.\nSince they are null terminated strings, we can give zero for the data length.\n\n",0);
+    C(handle,c,strlen(c));
+    C(handle,c,0);
+    C(handle,"\n",0);
+    H(handle,strdup(h),strlen(h));
+    H(handle,strdup(h),0);
+    {
+      C(handle,"\n\n"ANSI_INVERSE"Demonstrating exec"ANSI_RESET"\n",0);
+      char *cmd[]={"/usr/bin/date",NULL};
+      X(handle,0,cmd,NULL);
+    }
+    C(handle,"\n",1);
+    {
+      C(handle,"\n\n"ANSI_INVERSE"Demonstrating exec with environment variables"ANSI_RESET"\n",0);
+      char *cmd[]={"/usr/bin/bash","-c","set",NULL};
+      char *env[]={"my_environment_variable=Hello world",NULL};
+      X(handle,ZIPSFS_C_MMAP,cmd,env);
+    }
+    C(handle,"\n",1);
+    const int m_l=strlen(m);
+
+#define _config_c_to_mmap(bytes,size) memcpy(cg_mmap(0,size,0),bytes,size)
+    M(handle,_config_c_to_mmap(m,m_l),m_l);
+    M(handle,_config_c_to_mmap(m,m_l),0);
+    C(handle,"\n",1);
+    FSIZE_TO_HASHTABLE(handle,vp,vp_l);
+    return true;
+  }
+  return false;
+}
